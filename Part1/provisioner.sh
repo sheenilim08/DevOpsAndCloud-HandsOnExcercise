023db3b86a17a834745215e45a2bf3319e9ce94b
sheenilim08 <sheenismhael.lim@outlook.ph>
2023-07-03 05:45:16 +0800

    Added Docker Install

--- Part1/provisioner.sh
+++ Part1/provisioner.sh
@@ -5,6 +5,13 @@ sudo yum -y update
 
 # install git
 sudo yum -y  install git
+# install docker
+sudo amazon-linux-extras install docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+# docker service auto start
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
 
 # install SSM
 sudo yum -y  install https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
