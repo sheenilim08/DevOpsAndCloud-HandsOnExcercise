381d9247955dd7bd56702ba7b5bd2bb424aa6b24
sheenilim08 <sheenismhael.lim@outlook.ph>
2023-07-02 20:02:33 +0800

    Added -y to all yum commands

--- Part1/provisioner.sh
+++ Part1/provisioner.sh
@@ -4,14 +4,14 @@
 sudo yum -y update
 
 # install git
-sudo yum install git
+sudo yum -y  install git
 
 # install SSM
-sudo yum install https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+sudo yum -y  install https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 #sudo systemctl start amazon-ssm-agent
 
 # install cloudwatch agent
-sudo yum install https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
+sudo yum -y install https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a start
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a status
 
